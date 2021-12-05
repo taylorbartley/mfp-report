a19302c89f0bfc07ea23ab90b7309f01cf55f0bd
Taylor Bartley <tbartleyproj@gmail.com>
2021-12-05 12:27:42 +0000

    Lambda messenger

--- python/simple_request.py
+++ python/simple_request.py
@@ -1,3 +1,14 @@
-if __name__ == "__main__":
+import os
+import boto3
 
-    print("Don't forget to log noms today.")
+PUBLISH = {
+    "Message": "https://www.myfitnesspal.com/",
+    "Subject": "Don't forget to log noms today.",
+}
+
+
+def main(event, context):  # pylint: disable=unused-argument
+    """RemindeMe."""
+    topic_arn = os.environ["TOPIC_ARN"]
+    topic = boto3.resource("sns").Topic(arn=topic_arn)
+    topic.publish(**PUBLISH)
